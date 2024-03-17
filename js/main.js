c892b373fbef0e48ba02fb188215a7b3f411b1c5
max70453 <max70453@gmail.com>
2024-03-17 18:07:02 +0300

    fixes sections

--- js/main.js
+++ js/main.js
@@ -33,11 +33,11 @@
             
             $('html, body').animate({
                 scrollTop: $(this.hash).offset().top - 45
-            }, 1500, 'easeInOutExpo');
+            }, 'easeInOutExpo');
             
             if ($(this).parents('.navbar-nav').length) {
                 $('.navbar-nav .active').removeClass('active');
-                $(this).closest('a').addClass('active');
+                $(this).addClass('active');
             }
         }
     });
@@ -53,6 +53,8 @@
     });
     $('.back-to-top').click(function () {
         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('.navbar-nav .active').removeClass('active');
+        $(".navbar-nav #home").addClass('active');
         return false;
     });
     
